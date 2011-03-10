e8ec4317ec40df33e6dc6ab1d062680e25431c43
Édouard Brière <edouard.briere@gmail.com>
2011-03-10 09:52:11 +0100

    Fix specs

--- spec/web_translate_it/translation_file_spec.rb
+++ spec/web_translate_it/translation_file_spec.rb
@@ -18,28 +18,28 @@ describe WebTranslateIt::TranslationFile do
       file = mock(File)
       file.stub(:puts => true, :close => true)
       File.stub(:exist? => true, :mtime => Time.at(0), :new => file)
-      translation_file.fetch.should == "200 OK"
+      translation_file.fetch.should include "200 OK"
     end
     
     it "should prepare a HTTP request and get a 200 OK if the language file is stale using the force download parameter" do
       file = mock(File)
       file.stub(:puts => true, :close => true)
       File.stub(:exist? => true, :mtime => Time.at(0), :new => file)
-      translation_file.fetch(true).should == "200 OK"
+      translation_file.fetch(true).should include "200 OK"
     end
     
     it "should prepare a HTTP request and get a 304 OK if the language file is fresh" do
       file = mock(File)
       file.stub(:puts => true, :close => true)
       File.stub(:exist? => true, :mtime => Time.now, :new => file)
-      translation_file.fetch.should == "304 Not Modified"
+      translation_file.fetch.should include "304 Not Modified"
     end
     
     it "should prepare a HTTP request and get a 200 OK if the language file is fresh using the force download parameter" do
       file = mock(File)
       file.stub(:puts => true, :close => true)
       File.stub(:exist? => true, :mtime => Time.now, :new => file)
-      translation_file.fetch(true).should == "200 OK"
+      translation_file.fetch(true).should include "200 OK"
     end
   end
   
