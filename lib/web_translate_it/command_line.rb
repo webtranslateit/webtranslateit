5767ac40f12f30ec3205e472d62f65620c2c99d0
Édouard Brière <edouard.briere@gmail.com>
2012-04-18 17:48:46 +0200

    Update wording

--- lib/web_translate_it/command_line.rb
+++ lib/web_translate_it/command_line.rb
@@ -17,8 +17,6 @@ module WebTranslateIt
     def pull
       STDOUT.sync = true
       `#{configuration.before_pull}` if configuration.before_pull
-      puts StringUtil.titleize("Pulling files")
-
       # Selecting files to pull
       files = []
       fetch_locales_to_pull.each do |locale|
@@ -28,10 +26,10 @@ module WebTranslateIt
         puts "No files to pull."
       else
         # Now actually pulling files
+        puts "# Pulling files"
         time = Time.now
         threads = []
         n_threads = (files.count.to_f/3).ceil >= 20 ? 20 : (files.count.to_f/3).ceil
-        puts "Using up to #{n_threads} threads"
         ArrayUtil.chunk(files, n_threads).each do |file_array|
           unless file_array.empty?
             threads << Thread.new(file_array) do |file_array|
@@ -45,7 +43,7 @@ module WebTranslateIt
         end
         threads.each { |thread| thread.join }
         time = Time.now - time
-        puts "Pulled #{files.count} files at #{(files.count/time).round} files/sec."
+        puts "Pulled #{files.count} files at #{(files.count/time).round} files/sec, using #{n_threads} threads."
         `#{configuration.after_pull}` if configuration.after_pull
       end
     end
@@ -53,7 +51,7 @@ module WebTranslateIt
     def push
       STDOUT.sync = true
       `#{configuration.before_push}` if configuration.before_push
-      puts StringUtil.titleize("Pushing files")
+      puts "# Pushing files"
       WebTranslateIt::Connection.new(configuration.api_key) do |http|
         fetch_locales_to_push(configuration).each do |locale|
           configuration.files.find_all{ |file| file.locale == locale }.sort{|a,b| a.file_path <=> b.file_path} .each do |file|
@@ -67,8 +65,8 @@ module WebTranslateIt
     def add
       STDOUT.sync = true
       if parameters == []
-        puts StringUtil.failure("No master file given.")
-        puts "Usage: wti add master_file_1 master_file_2 ..."
+        puts StringUtil.failure("Error: You must provide the path to the master file to add.")
+        puts "Usage: wti add path/to/master_file_1 path/to/master_file_2 ..."
         exit
       end
       WebTranslateIt::Connection.new(configuration.api_key) do |http|
@@ -84,13 +82,13 @@ module WebTranslateIt
     def rm
       STDOUT.sync = true
       if parameters == []
-        puts StringUtil.failure("No master file given.")
-        puts "Usage: wti rm master_file_1 master_file_2 ..."
+        puts StringUtil.failure("Error: You must provide the path to the master file to remove.")
+        puts "Usage: wti path/to/rm master_file_1 path/to/master_file_2 ..."
         exit
       end
       WebTranslateIt::Connection.new(configuration.api_key) do |http|
         parameters.each do |param|
-          if Util.ask_yes_no("Are you certain you want to delete the master file #{param} and its attached target files and translations?", false)
+          if Util.ask_yes_no("Are you sure you want to delete the master file #{param}?\nThis will also delete its target files and translations.", false)
             configuration.files.find_all{ |file| file.file_path == param }.each do |master_file|
               master_file.delete(http)
               # delete files
@@ -108,35 +106,35 @@ module WebTranslateIt
     def addlocale
       STDOUT.sync = true
       if parameters == []
-        puts StringUtil.failure("No locale code given.")
-        puts "Usage: wti addlocale locale_code_1 locale_code_2 ..."
+        puts StringUtil.failure("Locale code missing.")
+        puts "Usage: wti addlocale fr es ..."
         exit
       end
       parameters.each do |param|
-        print StringUtil.success("Adding locale #{param}... ")
+        print StringUtil.success("Adding locale #{param.upcase}... ")
         WebTranslateIt::Connection.new(configuration.api_key) do
           puts WebTranslateIt::Project.create_locale(param)
         end
+        puts "Done."
       end
-      puts "Done!"
     end
     
     def rmlocale
       STDOUT.sync = true
       if parameters == []
-        puts StringUtil.failure("No locale code given.")
-        puts "Usage: wti rmlocale locale_code_1 locale_code_2 ..."
+        puts StringUtil.failure("Error: You must provide the locale code to remove.")
+        puts "Usage: wti rmlocale fr es ..."
         exit
       end
       parameters.each do |param|
-        if Util.ask_yes_no("Are you certain you want to delete the locale #{param} and its attached target files and translations?", false)
-          print StringUtil.success("Deleting locale #{param}... ")
+        if Util.ask_yes_no("Are you certain you want to delete the locale #{param.upcase}?\nThis will also delete its files and translations.", false)
+          print StringUtil.success("Deleting locale #{param.upcase}... ")
           WebTranslateIt::Connection.new(configuration.api_key) do |http|
             puts WebTranslateIt::Project.delete_locale(param)
           end
+          puts "Done."
         end
       end
-      puts "Done!"
     end
         
     def init
@@ -151,7 +149,7 @@ module WebTranslateIt
       end
       File.open(path, 'w'){ |file| file << generate_configuration(api_key, project_info) }
       puts ""
-      puts "Done! You can now use `wti` to push and pull your language files."
+      puts "Your project was successfully setup. You can now use `wti` to push and pull your language files."
       puts "Check `wti --help` for help."
     end
     
@@ -182,7 +180,7 @@ module WebTranslateIt
         stale = true if values['stale']
       end
       if stale
-        self.status if Util.ask_yes_no("Some of these stats are out of date. Would you like to refresh?", true)
+        self.status if Util.ask_yes_no("Some of these stats are outdated. Would you like to refresh?", true)
       end
     end
                 
