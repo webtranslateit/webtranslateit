e4ba14b879bcc06cea792d3d993e61b22547ec6b
Édouard Brière <edouard.briere@gmail.com>
2022-01-14 15:44:34 +0100

    Avoid key and term collisions in specs

--- spec/web_translate_it/string_spec.rb
+++ spec/web_translate_it/string_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 
 describe WebTranslateIt::String do
-  
-  let(:api_key) { "19875cfdd4f169b33e0ffab32cfb0bbb9e33d653" }
-  
+
+  let(:api_key) { "proj_pvt_glzDR250FLXlMgJPZfEyHQ" }
+
   describe "#initialize" do
     it "should assign api_key and many parameters" do
       string = WebTranslateIt::String.new({ "id" => 1234, "key" => "bacon"})
       string.id.should == 1234
       string.key.should == "bacon"
     end
-    
+
     it "should assign parameters using symbols" do
       string = WebTranslateIt::String.new({ :id => 1234, :key => "bacon"})
       string.id.should == 1234
       string.key.should == "bacon"
     end
   end
-  
+
   describe "#save" do
     it "should create a new String" do
       WebTranslateIt::Connection.new(api_key) do
@@ -31,7 +31,7 @@ describe WebTranslateIt::String do
         string.delete
       end
     end
-    
+
     it "should update an existing String" do
       WebTranslateIt::Connection.new(api_key) do
         string = WebTranslateIt::String.new({ "key" => "bacony" })
@@ -43,11 +43,11 @@ describe WebTranslateIt::String do
         string.delete
       end
     end
-    
+
     it "should create a new String with Translation" do
       translation1 = WebTranslateIt::Translation.new({ "locale" => "en", "text" => "Hello" })
       translation2 = WebTranslateIt::Translation.new({ "locale" => "fr", "text" => "Bonjour" })
-      
+
       string = WebTranslateIt::String.new({ "key" => "bacon", "translations" => [translation1, translation2] })
       WebTranslateIt::Connection.new(api_key) do
         string.save
@@ -60,20 +60,20 @@ describe WebTranslateIt::String do
         string.delete
       end
     end
-    
+
     it "should update a String and save its Translation" do
       translation1 = WebTranslateIt::Translation.new({ "locale" => "en", "text" => "Hello" })
       translation2 = WebTranslateIt::Translation.new({ "locale" => "fr", "text" => "Bonjour" })
-      
+
       string = WebTranslateIt::String.new({ "key" => "bacon" })
       WebTranslateIt::Connection.new(api_key) do
         string.save
         string_fetched = WebTranslateIt::String.find(string.id)
         string_fetched.translation_for("fr").should be_nil
-        
+
         string_fetched.translations = [translation1, translation2]
         string_fetched.save
-        
+
         string_fetched = WebTranslateIt::String.find(string.id)
         string_fetched.translation_for("en").should_not be_nil
         string_fetched.translation_for("en").text.should == "Hello"
@@ -83,7 +83,7 @@ describe WebTranslateIt::String do
       end
     end
   end
-  
+
   describe "#delete" do
     it "should delete a String" do
       string = WebTranslateIt::String.new({ "key" => "bacon" })
@@ -91,53 +91,48 @@ describe WebTranslateIt::String do
         string.save
         string_fetched = WebTranslateIt::String.find(string.id)
         string_fetched.should_not be_nil
-        
+
         string_fetched.delete
         WebTranslateIt::String.find(string.id).should be_nil
       end
     end
   end
-  
+
   describe "#find_all" do
     it "should find many strings" do
       WebTranslateIt::Connection.new(api_key) do
-        string1 = WebTranslateIt::String.new({ "key" => "bacon" })
+        string1 = WebTranslateIt::String.new({ "key" => "one two three" })
         string1.save
-        string2 = WebTranslateIt::String.new({ "key" => "tart" })
+        string2 = WebTranslateIt::String.new({ "key" => "four five six" })
         string2.save
-        string3 = WebTranslateIt::String.new({ "key" => "bacon tart" })
+        string3 = WebTranslateIt::String.new({ "key" => "six seven eight" })
         string3.save
-      
-        strings = WebTranslateIt::String.find_all({ "key" => "bacon" })
-        strings.count.should == 2
-        strings[0].key.should == "bacon"
-        strings[1].key.should == "bacon tart"
-        
-        strings = WebTranslateIt::String.find_all({ :key => "bacon" })
-        strings.count.should == 2
-        strings[0].key.should == "bacon"
-        strings[1].key.should == "bacon tart"
 
-        strings = WebTranslateIt::String.find_all({ "key" => "tart" })
+        strings = WebTranslateIt::String.find_all({ "key" => "six" })
         strings.count.should == 2
-        strings[0].key.should == "tart"
-        strings[1].key.should == "bacon tart"
-        
-        strings = WebTranslateIt::String.find_all({ "key" => "bacon tart" })
+        strings[0].key.should == "four five six"
+        strings[1].key.should == "six seven eight"
+
+        strings = WebTranslateIt::String.find_all({ :key => "six" })
+        strings.count.should == 2
+        strings[0].key.should == "four five six"
+        strings[1].key.should == "six seven eight"
+
+        strings = WebTranslateIt::String.find_all({ "key" => "eight" })
         strings.count.should == 1
-        strings[0].key.should == "bacon tart"
-        
-        string1.delete
-        string2.delete
-        string3.delete
+        strings[0].key.should == "six seven eight"
+
+        strings = WebTranslateIt::String.find_all({ "key" => "three" })
+        strings.count.should == 1
+        strings[0].key.should == "one two three"
       end
     end
   end
-  
+
   describe "#translation_for" do
     it "should fetch translations" do
       translation = WebTranslateIt::Translation.new({ "locale" => "en", "text" => "Hello" })
-      string = WebTranslateIt::String.new({ "key" => "bacon", "translations" => [translation] })
+      string = WebTranslateIt::String.new({ "key" => "greetings", "translations" => [translation] })
       WebTranslateIt::Connection.new(api_key) do
         string.save
         string_fetched = WebTranslateIt::String.find(string.id)
@@ -147,9 +142,9 @@ describe WebTranslateIt::String do
         string.delete
       end
     end
-    
+
     it "should not return a stale object" do
-      string = WebTranslateIt::String.new({ :key => "bacon" })
+      string = WebTranslateIt::String.new({ :key => "greetings 2" })
       translation = WebTranslateIt::Translation.new({ :locale => "es", :text => "text", :string_id => string.id })
       string.translations << translation
       string.translation_for('fr').should be_nil
--- spec/web_translate_it/term_spec.rb
+++ spec/web_translate_it/term_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe WebTranslateIt::Term do
-  
-  let(:api_key) { "19875cfdd4f169b33e0ffab32cfb0bbb9e33d653" }
-  
+
+  let(:api_key) { "proj_pvt_glzDR250FLXlMgJPZfEyHQ" }
+
   describe "#initialize" do
     it "should assign api_key and many parameters" do
       term = WebTranslateIt::Term.new({ "id" => 1234, "text" => "bacon"})
@@ -17,7 +17,7 @@ describe WebTranslateIt::Term do
       term.text.should == "bacon"
     end
   end
-  
+
   describe "#save" do
     it "should create a new Term" do
       WebTranslateIt::Connection.new(api_key) do
@@ -31,10 +31,10 @@ describe WebTranslateIt::Term do
         term.delete
       end
     end
-    
+
     it "should update an existing Term" do
       WebTranslateIt::Connection.new(api_key) do
-        term = WebTranslateIt::Term.new({ "text" => "Term", "description" => "A description" })
+        term = WebTranslateIt::Term.new({ "text" => "Term 2", "description" => "A description" })
         term.save
         term.text = "A Term"
         term.save
@@ -43,11 +43,11 @@ describe WebTranslateIt::Term do
         term.delete
       end
     end
-    
+
     it "should create a new Term with a TermTranslation" do
       translation1 = WebTranslateIt::TermTranslation.new({ "locale" => "fr", "text" => "Bonjour" })
       translation2 = WebTranslateIt::TermTranslation.new({ "locale" => "fr", "text" => "Salut" })
-      
+
       term = WebTranslateIt::Term.new({ "text" => "Hello", "translations" => [translation1, translation2] })
       WebTranslateIt::Connection.new(api_key) do
         term.save
@@ -59,20 +59,20 @@ describe WebTranslateIt::Term do
         term.delete
       end
     end
-    
+
     it "should update a Term and save its Translation" do
       translation1 = WebTranslateIt::TermTranslation.new({ "locale" => "fr", "text" => "Bonjour" })
       translation2 = WebTranslateIt::TermTranslation.new({ "locale" => "fr", "text" => "Salut" })
-      
+
       term = WebTranslateIt::Term.new({ "text" => "Hi!" })
       WebTranslateIt::Connection.new(api_key) do
         term.save
         term_fetched = WebTranslateIt::Term.find(term.id)
         term_fetched.translation_for("fr").should be_nil
-        
+
         term_fetched.translations = [translation1, translation2]
         term_fetched.save
-        
+
         term_fetched = WebTranslateIt::Term.find(term.id)
         term_fetched.translation_for("fr").should_not be_nil
         term_fetched.translation_for("fr")[0].text.should == "Bonjour"
@@ -81,7 +81,7 @@ describe WebTranslateIt::Term do
       end
     end
   end
-  
+
   describe "#delete" do
     it "should delete a Term" do
       term = WebTranslateIt::Term.new({ "text" => "bacon" })
@@ -89,27 +89,27 @@ describe WebTranslateIt::Term do
         term.save
         term_fetched = WebTranslateIt::Term.find(term.id)
         term_fetched.should_not be_nil
-        
+
         term_fetched.delete
         term_fetched = WebTranslateIt::Term.find(term.id)
         term_fetched.should be_nil
       end
     end
   end
-  
+
   describe "#find_all" do
     it "should fetch many terms" do
       WebTranslateIt::Connection.new(api_key) do
-        term1 = WebTranslateIt::Term.new({ "text" => "bacon" })
+        term1 = WebTranslateIt::Term.new({ "text" => "greeting 1" })
         term1.save
-        term2 = WebTranslateIt::Term.new({ "text" => "tart" })
+        term2 = WebTranslateIt::Term.new({ "text" => "greeting 2" })
         term2.save
-        term3 = WebTranslateIt::Term.new({ "text" => "bacon tart" })
+        term3 = WebTranslateIt::Term.new({ "text" => "greeting 3" })
         term3.save
-      
+
         terms = WebTranslateIt::Term.find_all
         terms.count.should == 3
-        
+
         term1.delete
         term2.delete
         term3.delete
