06029e71eba0c6d3a3d4800bd449704c84a0967f
Édouard Brière <edouard.briere@gmail.com>
2022-03-08 12:03:37 +0100

    Fix Lint/AmbiguousOperatorPrecedence offence

--- lib/web_translate_it/util/array_util.rb
+++ lib/web_translate_it/util/array_util.rb
@@ -15,7 +15,7 @@ class ArrayUtil
     1.upto(pieces) do |i|
       last = start + mid
       last = last - 1 unless len % pieces >= i
-      chunks << arr[start..last] || []
+      chunks << (arr[start..last] || [])
       start = last + 1
     end
     chunks
