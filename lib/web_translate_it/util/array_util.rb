ee1717e77cd97445a29c53937a800cf7de08a51d
Édouard Brière <edouard.briere@gmail.com>
2022-06-14 11:57:12 +0200

    Fix rubocop offences

--- lib/web_translate_it/util/array_util.rb
+++ lib/web_translate_it/util/array_util.rb
@@ -1,7 +1,7 @@
 class ArrayUtil
   def self.to_columns(arr)
     if arr[0][0] == '*'
-      "*#{StringUtil.backward_truncate(arr[0][1..-1])} | #{arr[1]}  #{arr[2]}\n"
+      "*#{StringUtil.backward_truncate(arr[0][1..])} | #{arr[1]}  #{arr[2]}\n"
     else
       " #{StringUtil.backward_truncate(arr[0])} | #{arr[1]}  #{arr[2]}\n"
     end
