411a9be14d7a97be0cd0cb2b5fcf30ef25ff81c5
Roger Oba <rogerluan.oba@gmail.com>
2023-04-16 14:12:23 -0300

    Fix crash caused by use of API from older Multipart versions.

--- lib/web_translate_it/translation_file.rb
+++ lib/web_translate_it/translation_file.rb
@@ -96,7 +96,7 @@ module WebTranslateIt
       if File.exist?(file_path)
         if force || (remote_checksum != local_checksum)
           File.open(file_path) do |file|
-            params = {'file' => ::Multipart::Post::UploadIO.new(file, 'text/plain', file.path), 'merge' => merge, 'ignore_missing' => ignore_missing, 'label' => label, 'minor_changes' => minor_changes}
+            params = {'file' => UploadIO.new(file, 'text/plain', file.path), 'merge' => merge, 'ignore_missing' => ignore_missing, 'label' => label, 'minor_changes' => minor_changes}
             params['name'] = destination_path unless destination_path.nil?
             params['rename_others'] = rename_others
             request = Net::HTTP::Put::Multipart.new(api_url, params)
@@ -147,7 +147,7 @@ module WebTranslateIt
       display.push "#{StringUtil.checksumify(local_checksum.to_s)}..[     ]"
       if File.exist?(file_path)
         File.open(file_path) do |file|
-          request = ::Net::HTTP::Post::Multipart.new(api_url_for_create, {'name' => file_path, 'file' => Multipart::Post::UploadIO.new(file, 'text/plain', file.path)})
+          request = Net::HTTP::Post::Multipart.new(api_url_for_create, {'name' => file_path, 'file' => UploadIO.new(file, 'text/plain', file.path)})
           WebTranslateIt::Util.add_fields(request)
           display.push Util.handle_response(http_connection.request(request))
           puts ArrayUtil.to_columns(display)
