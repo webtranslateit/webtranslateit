webtranslateit/webtranslateit

commit 6bcf41c129f4c971c5cd1986e35579e5240f938e

Fix Style/ClassVars offences

.rubocop_todo.yml
@@ -30,25 +30,6 @@ Naming/AccessorMethodName:
   Exclude:
     - 'lib/web_translate_it/configuration.rb'
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-# Offense count: 7
-# Cop supports --auto-correct.
-# Configuration parameters: EnforcedStyle.
-# SupportedStyles: always, conditionals
-Style/AndOr:
-  Exclude:
-    - 'lib/web_translate_it/configuration.rb'
-    - 'lib/web_translate_it/translation_file.rb'
-    - 'lib/web_translate_it/util.rb'
-
-=======
->>>>>>> a65ec25 (Fix Style/AndOr offences)
-# Offense count: 9
-Style/ClassVars:
-  Exclude:
-    - 'lib/web_translate_it/connection.rb'
-=======
 # Offense count: 15
 # Cop supports --auto-correct.
 Security/YAMLLoad:
@@ -59,7 +40,6 @@ Security/YAMLLoad:
     - 'lib/web_translate_it/string.rb'
     - 'lib/web_translate_it/term.rb'
     - 'lib/web_translate_it/term_translation.rb'
->>>>>>> 0768576 (Revert "Fix Security/YAMLLoad offences")
 
 # Offense count: 14
 # Configuration parameters: AllowedConstants.

lib/web_translate_it/connection.rb
@@ -1,15 +1,17 @@
 module WebTranslateIt
   class Connection
+    attr_reader :api_key, :http_connection
+
     require 'net/http'
     require 'net/https'
     require 'openssl'
     require 'uri'
     require 'ostruct'
 
-    @@api_key = nil
-    @@http_connection = nil
-    @@debug = false
-    @@silent = false
+    @api_key = nil
+    @http_connection = nil
+    @debug = false
+    @silent = false
 
     #
     # Initialize and yield a HTTPS Keep-Alive connection to WebTranslateIt.com
@@ -27,44 +29,36 @@ module WebTranslateIt
     # end
     #
     def initialize(api_key) # rubocop:todo Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/MethodLength
-      @@api_key = api_key
+      @api_key = api_key
       proxy = ENV['http_proxy'] ? URI.parse(ENV['http_proxy']) : OpenStruct.new
       http = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password).new('webtranslateit.com', 443)
       http.use_ssl      = true
       http.open_timeout = http.read_timeout = 60
-      http.set_debug_output($stderr) if @@debug
+      http.set_debug_output($stderr) if @debug
       begin
-        http.verify_mode  = OpenSSL::SSL::VERIFY_PEER
-        @@http_connection = http.start
-        yield @@http_connection if block_given?
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        @http_connection = http.start
+        yield @http_connection if block_given?
       rescue OpenSSL::SSL::SSLError
-        puts 'Unable to verify SSL certificate.' unless @@silent
+        puts 'Unable to verify SSL certificate.' unless @silent
         http = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password).new('webtranslateit.com', 443)
-        http.set_debug_output($stderr) if @@debug
+        http.set_debug_output($stderr) if @debug
         http.use_ssl      = true
         http.open_timeout = http.read_timeout = 60
-        http.verify_mode  = OpenSSL::SSL::VERIFY_NONE
-        @@http_connection = http.start
-        yield @@http_connection if block_given?
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        @http_connection = http.start
+        yield @http_connection if block_given?
       rescue
         puts $!
       end
     end
 
-    def self.http_connection
-      @@http_connection
-    end
-
     def self.turn_debug_on
-      @@debug = true
+      @debug = true
     end
 
     def self.turn_silent_on
-      @@silent = true
-    end
-
-    def self.api_key
-      @@api_key
+      @silent = true
     end
   end
 end

commit 7ff429c1a9b25f07036d1582d6fd48b40344c12c

Fix wti addlocale and wti rmlocale commands, broken in 6bcf41c

.rubocop_todo.yml
@@ -1,6 +1,6 @@
 # This configuration was generated by
 # `rubocop --auto-gen-config --exclude-limit 100`
-# on 2023-03-13 12:52:17 UTC using RuboCop version 1.48.0.
+# on 2023-04-19 11:01:25 UTC using RuboCop version 1.50.2.
 # The point is for the user to remove these configuration records
 # one by one as the offenses are removed from the code base.
 # Note that changes in the inspected code, or installation of new
@@ -28,6 +28,11 @@ RSpec/NoExpectationExample:
 RSpec/VerifiedDoubles:
   Exclude:
     - 'spec/web_translate_it/auto_fetch_spec.rb'
+
+# Offense count: 7
+Style/ClassVars:
+  Exclude:
+    - 'lib/web_translate_it/connection.rb'
 
 # Offense count: 27
 # This cop supports unsafe autocorrection (--autocorrect-all).

lib/web_translate_it/connection.rb
@@ -2,11 +2,9 @@ module WebTranslateIt
 
   class Connection
 
-    attr_reader :api_key, :http_connection
-
-    @api_key = nil
-    @http_connection = nil
-    @debug = false
+    @@api_key = nil
+    @@http_connection = nil
+    @@debug = false
 
     #
     # Initialize and yield a HTTPS Keep-Alive connection to WebTranslateIt.com
@@ -24,32 +22,40 @@ module WebTranslateIt
     # end
     #
     def initialize(api_key) # rubocop:todo Metrics/AbcSize, Metrics/MethodLength
-      @api_key = api_key
+      @@api_key = api_key
       proxy = ENV['http_proxy'] ? URI.parse(ENV['http_proxy']) : Struct.new(:host, :port, :user, :password).new
       http = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password).new('webtranslateit.com', 443)
       http.use_ssl      = true
       http.open_timeout = http.read_timeout = 60
-      http.set_debug_output($stderr) if @debug
+      http.set_debug_output($stderr) if @@debug
       begin
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        @http_connection = http.start
-        yield @http_connection if block_given?
+        @@http_connection = http.start
+        yield @@http_connection if block_given?
       rescue OpenSSL::SSL::SSLError
         puts 'Unable to verify SSL certificate.'
         http = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password).new('webtranslateit.com', 443)
-        http.set_debug_output($stderr) if @debug
+        http.set_debug_output($stderr) if @@debug
         http.use_ssl      = true
         http.open_timeout = http.read_timeout = 60
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        @http_connection = http.start
-        yield @http_connection if block_given?
+        @@http_connection = http.start
+        yield @@http_connection if block_given?
       rescue StandardError
         puts $ERROR_INFO
       end
     end
 
     def self.turn_debug_on
-      @debug = true
+      @@debug = true
+    end
+
+    def self.api_key
+      @@api_key
+    end
+
+    def self.http_connection
+      @@http_connection
     end
 
   end
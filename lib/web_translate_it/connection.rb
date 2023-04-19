7ff429c1a9b25f07036d1582d6fd48b40344c12c
Édouard Brière <edouard.briere@gmail.com>
2023-04-19 13:01:52 +0200

    Fix wti addlocale and wti rmlocale commands, broken in 6bcf41c

--- lib/web_translate_it/connection.rb
+++ lib/web_translate_it/connection.rb
@@ -2,11 +2,9 @@ module WebTranslateIt
 
   class Connection
 
-    attr_reader :api_key, :http_connection
-
-    @api_key = nil
-    @http_connection = nil
-    @debug = false
+    @@api_key = nil
+    @@http_connection = nil
+    @@debug = false
 
     #
     # Initialize and yield a HTTPS Keep-Alive connection to WebTranslateIt.com
@@ -24,32 +22,40 @@ module WebTranslateIt
     # end
     #
     def initialize(api_key) # rubocop:todo Metrics/AbcSize, Metrics/MethodLength
-      @api_key = api_key
+      @@api_key = api_key
       proxy = ENV['http_proxy'] ? URI.parse(ENV['http_proxy']) : Struct.new(:host, :port, :user, :password).new
       http = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password).new('webtranslateit.com', 443)
       http.use_ssl      = true
       http.open_timeout = http.read_timeout = 60
-      http.set_debug_output($stderr) if @debug
+      http.set_debug_output($stderr) if @@debug
       begin
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        @http_connection = http.start
-        yield @http_connection if block_given?
+        @@http_connection = http.start
+        yield @@http_connection if block_given?
       rescue OpenSSL::SSL::SSLError
         puts 'Unable to verify SSL certificate.'
         http = Net::HTTP::Proxy(proxy.host, proxy.port, proxy.user, proxy.password).new('webtranslateit.com', 443)
-        http.set_debug_output($stderr) if @debug
+        http.set_debug_output($stderr) if @@debug
         http.use_ssl      = true
         http.open_timeout = http.read_timeout = 60
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-        @http_connection = http.start
-        yield @http_connection if block_given?
+        @@http_connection = http.start
+        yield @@http_connection if block_given?
       rescue StandardError
         puts $ERROR_INFO
       end
     end
 
     def self.turn_debug_on
-      @debug = true
+      @@debug = true
+    end
+
+    def self.api_key
+      @@api_key
+    end
+
+    def self.http_connection
+      @@http_connection
     end
 
   end
