7376c0e30f822efca5e02db8314f70dc480cbeff
Édouard Brière <edouard.briere@gmail.com>
2026-03-02 10:21:31 +0100

    Fix rubocop offence

---
+++ lib/web_translate_it/util/hash_extensions.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Hash
+
+  # Destructively convert all keys to strings.
+  def stringify_keys!
+    keys.each do |key|
+      self[key.to_s] = delete(key)
+    end
+    self
+  end
+
+end
--- lib/web_translate_it/util/hash_util.rb
+++ lib/web_translate_it/util/hash_util.rb
@@ -30,14 +30,4 @@ class HashUtil
 
 end
 
-class Hash
-
-  # Destructively convert all keys to strings.
-  def stringify_keys!
-    keys.each do |key|
-      self[key.to_s] = delete(key)
-    end
-    self
-  end
-
-end
+require_relative 'hash_extensions'
